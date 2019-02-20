741963fa235c71f2998c82ec6225683e0cb4e074
Mekan <164226761@qq.com>
2019-02-20 20:46:29 +0800

    把本地工程添加到git

---
+++ src/cn/itcast/git/gitDemo01.java
@@ -0,0 +1,10 @@
+package cn.itcast.git;
+
+public class gitDemo01 {
+    public static void main(String[] args) {
+        System.out.println("hello git !");
+        System.out.println("hello git !");
+        System.out.println("hello git !");
+        System.out.println("hello git !");
+    }
+}
